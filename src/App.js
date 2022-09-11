00ee5ec2bd9e5db5ed6f826231ac5b6b24efd89d
Yohanes O.L <yohanes.lumentut@gmail.com>
2022-09-11 14:43:43 +0700

    feat: Implement gmaps-script-loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { useLoadScript } from '@react-google-maps/api';
+import { useScriptLoader } from 'gmaps-script-loader';
 import { mapScriptLoaded } from './features/map/store/actions';
 import { useMapContextProvider, MapContext } from './features/map';
 import HomePage from './pages/Home';
@@ -16,16 +16,20 @@ function App() {
   const dispatch = useDispatch();
   const map = useMapContextProvider();
 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  const { loadScript, isMapReady } = useScriptLoader({
+    apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
 
   useEffect(() => {
-    if (isLoaded) {
+    loadScript('id', 'ID');
+  }, [loadScript]);
+
+  useEffect(() => {
+    if (isMapReady) {
       dispatch(mapScriptLoaded());
     }
-  }, [dispatch, isLoaded]);
+  }, [dispatch, isMapReady]);
 
   return (
     <MapContext.Provider value={map}>
